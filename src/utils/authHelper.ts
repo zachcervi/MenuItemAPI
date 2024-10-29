6fc81af93614e96d6c295a64e51209544e428905
Zach Cervi <zachcervi@gmail.com>
2024-10-29 13:59:21 -0600

    prettier

--- src/utils/authHelper.ts
+++ src/utils/authHelper.ts
@@ -4,15 +4,12 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export async function getAuthToken() {
-  const { data } = await axios.post(
-    `${process.env.AUTH0_DOMAIN}/oauth/token`,
-    {
-      client_id: process.env.AUTH0_CLIENT_ID,
-      client_secret: process.env.AUTH0_CLIENT_SECRET,
-      audience: process.env.AUTH0_AUDIENCE,
-      grant_type: 'client_credentials',
-    },
-  );
+  const { data } = await axios.post(`${process.env.AUTH0_DOMAIN}/oauth/token`, {
+    client_id: process.env.AUTH0_CLIENT_ID,
+    client_secret: process.env.AUTH0_CLIENT_SECRET,
+    audience: process.env.AUTH0_AUDIENCE,
+    grant_type: 'client_credentials',
+  });
   console.log('DATA', data);
 
   return data.access_token;
