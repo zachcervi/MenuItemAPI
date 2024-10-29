c20643c9c59df4f0f399573cd1612d043ad9f7ab
Zach Cervi <zachcervi@gmail.com>
2024-10-29 10:38:40 -0600

    added error handling

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,8 @@ const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const items_router_1 = require("./items/items.router");
+const error_middleware_1 = require("./middleware/error.middleware");
+const not_found_middleware_1 = require("./middleware/not-found.middleware");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
@@ -15,6 +17,8 @@ app.use((0, helmet_1.default)());
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use("/api/menu/items", items_router_1.itemsRouter);
+app.use(error_middleware_1.errorHandler);
+app.use(not_found_middleware_1.notFoundHandler);
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
 });
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import { itemsRouter } from "./items/items.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
 
 dotenv.config();
 
@@ -13,6 +15,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use("/api/menu/items", itemsRouter);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
