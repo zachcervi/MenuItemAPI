bd22f520e9575c76a25ea2375e433261148e78a8
Zach Cervi <zachcervi@gmail.com>
2024-10-29 10:33:37 -0600

    CRUD Menu Items

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const helmet_1 = __importDefault(require("helmet"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const items_router_1 = require("./items/items.router");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
+app.use((0, helmet_1.default)());
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use("/api/menu/items", items_router_1.itemsRouter);
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
 import dotenv from "dotenv";
+import { itemsRouter } from "./items/items.router";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use("/api/menu/items", itemsRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
